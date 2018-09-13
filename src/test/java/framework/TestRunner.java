2eb87b808b4171b532b641abde75256d52046835
ilir ahmeti <ilirahmeti@yahoo.com>
2018-09-13 12:23:27 -0400

    Update TestRunner.java

--- src/test/java/framework/TestRunner.java
+++ src/test/java/framework/TestRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         snippets = SnippetType.CAMELCASE,
 
         features = {"src/test/Resources/Features"},
-        tags = {"@Darksky-2"},
+        tags = {"@Darksky-1"},
         glue = {"stepdefinition"}
 )
 
